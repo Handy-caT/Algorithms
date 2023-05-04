d9a457b3bd6a67a7604aaa065ea4c32b8135662c
Maxim Volkov <37216852+Handy-caT@users.noreply.github.com>
2023-05-04 21:30:49 +0200

    added functions find next square

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,7 +2,9 @@
 // Created by Максим on 4.05.23.
 //
 #include "reverse_words.cpp"
+#include "find_next_square.cpp"
 
 int main() {
     custom_functions_tests::reverse_words_test();
+    custom_functions_tests::find_next_square_test();
 }
